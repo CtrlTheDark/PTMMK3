506b816e3098aa4d20b6a4bf00141f6e5d8c1e3c
ctrlthedark <ctrlthedark@gmail.com>
2014-11-05 21:51:05 -0500

    first com, trying to get titles to appear

---
+++ MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  PlayTheMarketMK3
+//
+//  Created by Brandon Fink on 11/5/14.
+//  Copyright (c) 2014 Brandon Fink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
